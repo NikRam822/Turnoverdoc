c6372241700440b2726152f0d0576139e60d5b3d
Ramzin Nikita <rivorn73@gmail.com>
2022-10-08 20:03:28 +0500

    Right set cookie

--- src/main/java/com/turnoverdoc/turnover/controllers/AuthController.java
+++ src/main/java/com/turnoverdoc/turnover/controllers/AuthController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class AuthController {
 
@@ -13,6 +17,18 @@ public class AuthController {
     public ResponseEntity greeting(@RequestHeader("login") String login, @RequestHeader("password") String password) {
         // code that uses the language variable
         return new ResponseEntity("your login: "+ login +"\nyour password: "+ password, HttpStatus.OK);
+    }
+
+    //записать куки
+    @GetMapping(value = "/set-cookie")
+    public ResponseEntity<?> setCookie(HttpServletResponse response) throws IOException {
+        Cookie cookie = new Cookie("data", "Come_to_the_dark_side");//создаем объект Cookie,
+        //в конструкторе указываем значения для name и value
+        cookie.setPath("/");//устанавливаем путь
+        cookie.setMaxAge(86400);//здесь устанавливается время жизни куки
+        response.addCookie(cookie);//добавляем Cookie в запрос
+        response.setContentType("text/plain");//устанавливаем контекст
+        return ResponseEntity.ok().body(HttpStatus.OK);//получилось как бы два раза статус ответа установили, выбирайте какой вариант лучше
     }
 
 }
