65a2a2ff0674fccc02d61fcb41f42ffc70aa4f43
Ramzin Nikita <rivorn73@gmail.com>
2022-10-08 19:30:48 +0500

    Add auth controller

---
+++ src/main/java/com/turnoverdoc/turnover/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.turnoverdoc.turnover.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/greeting")
+    public ResponseEntity greeting(@RequestHeader("login") String login, @RequestHeader("password") String password) {
+        // code that uses the language variable
+        return new ResponseEntity("your login: "+ login +"\nyour password: "+ password, HttpStatus.OK);
+    }
+
+}
