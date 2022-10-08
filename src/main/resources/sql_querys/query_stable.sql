32038d7917f81f40c75c8a52274d4700fa36a59e
Timur-Akhmatov-tch <t.akhmatovya@yandex.ru>
2022-10-08 16:02:51 +0500

    Added script for generating DB tables

---
+++ src/main/resources/sql_querys/query_stable.sql
@@ -0,0 +1,22 @@
+DROP TABLE main.request, main.user, main.contact;
+CREATE TABLE main.user(
+	id SERIAL PRIMARY KEY,
+	login TEXT UNIQUE NOT NULL CHECK(login !=''),
+	password TEXT NOT NULL CHECK(password !=''),
+	access INTEGER DEFAULT 0
+);
+CREATE TABLE main.contact(
+	id SERIAL PRIMARY KEY,
+	phone TEXT NOT NULL,
+	messanger TEXT,
+	email TEXT NOT NULL
+);
+CREATE TABLE main.request(
+	id SERIAL PRIMARY KEY,
+	id_user INTEGER REFERENCES main.user(id),
+	id_contact INTEGER REFERENCES main.contact(id),
+	full_name TEXT NOT NULL CHECK(full_name !=''),
+	photo_url TEXT NOT NULL CHECK(photo_url !=''),
+	pdf_url TEXT NOT NULL CHECK(pdf_url !=''),
+	status INTEGER DEFAULT 0
+);\
