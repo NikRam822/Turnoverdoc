e9b690afef3105aa984d8822227b40db3cde5e9b
Ramzin Nikita <rivorn73@gmail.com>
2022-10-16 20:39:09 +0500

    Sql script update

--- src/main/resources/sql_querys/query_stable.sql
+++ src/main/resources/sql_querys/query_stable.sql
@@ -14,13 +14,13 @@ CREATE TABLE IF NOT EXISTS public.contacts
 CREATE TABLE IF NOT EXISTS public.orders
 (
     id bigint NOT NULL,
-    fullname text COLLATE pg_catalog."default" NOT NULL,
-    pasport_path text COLLATE pg_catalog."default" NOT NULL,
+    passport_path text COLLATE pg_catalog."default" NOT NULL,
     p45_path text COLLATE pg_catalog."default",
     p60_path text COLLATE pg_catalog."default",
     p85_path text COLLATE pg_catalog."default",
     contract_path text COLLATE pg_catalog."default",
     status text COLLATE pg_catalog."default" NOT NULL,
+    id_user bigint,
     CONSTRAINT orders_pkey PRIMARY KEY (id)
     );
 
@@ -30,12 +30,6 @@ CREATE TABLE IF NOT EXISTS public.roles
     name character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT roles_pkey PRIMARY KEY (id)
     );
-
-CREATE TABLE IF NOT EXISTS public.user_orders
-(
-    user_id bigint NOT NULL,
-    order_id bigint NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS public.user_roles
 (
@@ -62,16 +56,8 @@ CREATE INDEX IF NOT EXISTS contacts_pkey
     ON public.contacts(id_order);
 
 
-ALTER TABLE IF EXISTS public.user_orders
-    ADD CONSTRAINT order_id FOREIGN KEY (order_id)
-    REFERENCES public.orders (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-       ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.user_orders
-    ADD CONSTRAINT user_key FOREIGN KEY (user_id)
+ALTER TABLE IF EXISTS public.orders
+    ADD CONSTRAINT user_key FOREIGN KEY (id_user)
     REFERENCES public.users (id) MATCH SIMPLE
     ON UPDATE NO ACTION
        ON DELETE NO ACTION
