30e8fa817c40efb0c48ccec7ecfc2a8b3519a1cf
Timur-Akhmatov-tch <tg.akhmatov@gmail.com>
2022-12-03 02:07:21 +0500

    Created Export orders to csv

--- src/main/java/com/turnoverdoc/turnover/services/ExportService.java
+++ src/main/java/com/turnoverdoc/turnover/services/ExportService.java
@@ -2,8 +2,9 @@ package com.turnoverdoc.turnover.services;
 
 import com.turnoverdoc.turnover.model.Order;
 
+import java.io.Writer;
 import java.util.List;
 
 public interface ExportService {
-    void writeOrderToCsv();
+    void writeOrderToCsv(Writer writer);
 }
--- src/main/java/com/turnoverdoc/turnover/services/impl/ExportService.java
+++ src/main/java/com/turnoverdoc/turnover/services/impl/ExportService.java
@@ -1,11 +1,51 @@
 package com.turnoverdoc.turnover.services.impl;
 
+import com.turnoverdoc.turnover.model.Order;
+import com.turnoverdoc.turnover.repositories.OrderRepository;
+import com.turnoverdoc.turnover.services.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ExportService implements com.turnoverdoc.turnover.services.ExportService {
-    @Override
-    public void writeOrderToCsv() {
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
 
+@Service
+@Slf4j
+public class ExportService implements com.turnoverdoc.turnover.services.ExportService {
+
+    private final Logger LOGGER = log;
+    private final OrderService orderService;
+
+    @Autowired
+    public ExportService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @Override
+    public void writeOrderToCsv(Writer writer) {
+        List<Order> orders = orderService.getAll();
+        try (CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+            csvPrinter.printRecord("ID", "STATUS", "USER_ID", "USERNAME", "PASSPORT", "P45", "P60", "P80", "CONTRACT");
+            for (Order order : orders) {
+                if(order.getUser()!=null) {
+                    csvPrinter.printRecord(order.getId(),
+                            order.getStatus(),
+                            order.getUser().getId(),
+                            order.getUser().getUsername(),
+                            order.getPassportPath(),
+                            order.getP45Path(),
+                            order.getP60Path(),
+                            order.getP80Path(),
+                            order.getContractPath());
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error While writing CSV ", e);
+        }
     }
 }
