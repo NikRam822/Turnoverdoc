9162a0e428e2de95015bca2cfb215a61d4691bc4
Ramzin Nikita <rivorn73@gmail.com>
2022-12-17 23:24:53 +0500

    Rename global controller

--- src/main/java/com/turnoverdoc/turnover/controllers/ErrorWrapperController.java
+++ src/main/java/com/turnoverdoc/turnover/controllers/ErrorWrapperController.java
@@ -3,15 +3,11 @@ package com.turnoverdoc.turnover.controllers;
 import com.turnoverdoc.turnover.error.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ResourceBundle;
-
 @RestControllerAdvice
-public class GlobalController {
+public class ErrorWrapperController {
     @ExceptionHandler(ErrorDto.class)
     public ResponseEntity<String> handleError(ErrorDto ex) {
         return new ResponseEntity<>("There was an error: " + ex.getDescription(), HttpStatus.CONFLICT);
