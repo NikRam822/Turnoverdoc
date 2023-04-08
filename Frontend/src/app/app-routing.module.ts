d396c444a3815292c527950801fe5441b442c0dd
Nikita Ramzin <rivorn73@gmail.com>
2023-04-08 20:10:53 +0500

    Guards

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {AuthComponent} from "./auth/auth.component";
+import {LoginPageGuard} from "./guards/login-page.guard";
+import {AuthGuard} from "./guards/autn-page.guard";
 
 
 
 const appRoutes: Routes = [
-  {path: 'login', component: LoginComponent/*, canActivate: [true]*/ },
-  {path: 'auth', component: AuthComponent/*, canActivate: [AuthGuard]*/},
+  {path: 'login', component: LoginComponent, canActivate: [LoginPageGuard] },
+  {path: 'auth', component: AuthComponent, canActivate: [AuthGuard]},
 ];
 
 
