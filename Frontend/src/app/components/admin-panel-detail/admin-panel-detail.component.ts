b1ce2dcbf3c104e55c4c0486a9ee3ee2c1d6234a
Nikita Ramzin <rivorn73@gmail.com>
2023-04-22 18:45:09 +0500

    order-detail for admin

--- Frontend/src/app/components/admin-panel-detail/admin-panel-detail.component.ts
+++ Frontend/src/app/components/admin-panel-detail/admin-panel-detail.component.ts
@@ -24,6 +24,73 @@ export class AdminPanelDetailComponent {
     this.router.navigate(['/admin-panel']);
   }
 
+  filesMap = new Map();
+  declare selectedFiles: FileList;
+
+  selectFile(fileName: String, {event}: { event: any }) {
+    this.selectedFiles = event.target.files;
+    this.filesMap.set(fileName, this.selectedFiles.item(0))
+  }
+
+  upload() {
+    this.httpService.postUploadDocs(this.orderStore.getValue().fullOrder?.id, this.filesMap).subscribe({
+      next: (response) => {
+        this.router.navigate(['/admin-panel'])
+      },
+      error: (error) => {
+        if (error.status === 200) {
+          this.router.navigate(['/admin-panel'])
+        } else {
+          console.error(error);
+        }
+      }
+    });
+
+  }
+
+  download(fileName: String) {
+    this.httpService.getDownloadDocs(this.orderStore.getValue().fullOrder?.id, fileName).subscribe({
+      next: (response) => {
+        alert("Ok")
+      },
+      error: (error) => {
+        if (error.status === 200) {
+          location.href = "http://localhost:8080/api/order/download/" + this.orderStore.getValue().fullOrder?.id + "/" + fileName
+        } else {
+          console.error(error);
+        }
+      }
+    });
+  }
+
+  disableButton(fileName: String) {
+    if (fileName === "PASSPORT") {
+      return this.orderStore.getValue().fullOrder?.passportPath === null;
+    }
+
+    if (fileName === "CONTRACT") {
+      return this.orderStore.getValue().fullOrder?.contractPath === null;
+    }
+
+    if (fileName === "P_45") {
+      return this.orderStore.getValue().fullOrder?.p45Path === null;
+    }
+
+    if (fileName === "P_60") {
+      return this.orderStore.getValue().fullOrder?.p60Path === null;
+    }
+
+    if (fileName === "P_80") {
+      return this.orderStore.getValue().fullOrder?.p80Path === null;
+    }
+    return true;
+  }
+
+  disableUploadButton(){
+    return !(this.filesMap.get('CONTRACT') && this.filesMap.get('PASSPORT'))
+  }
+
+
   submit() {
     this.httpService.postAdminChangeStatus(String(this.orderStore.getValue().fullOrder.id), String(this.status)).subscribe({
       next: (data) => {
--- Frontend/src/app/models/orders.ts
+++ Frontend/src/app/models/orders.ts
@@ -16,4 +16,10 @@ export interface FullOrder {
   status: String
   date: number
   username:string
+
+  p45Path: String
+  p60Path: String
+  p80Path: String
+  passportPath: String
+  contractPath:String
 }
